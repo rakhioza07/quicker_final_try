84e843543451650b07c191bc5f311086647ba43b
rakhioza07 <rakhioza07@gmail.com>
2017-07-25 23:22:35 +0530

    25/7 raat ke last changes

---
+++ slide_left_right.js
@@ -0,0 +1,16 @@
+
+function openNav1() {
+    document.getElementById("mySidenav1").style.width = "100%";
+}
+
+function closeNav1() {
+    document.getElementById("mySidenav1").style.width = "0";
+}
+
+function openNav2() {
+    document.getElementById("mySidenav2").style.width = "100%";
+}
+
+function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
+}
